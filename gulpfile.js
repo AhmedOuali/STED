93ca12c63b16ca999f0eb314a6593749682a1d36
Ahmed <ahmed.ouali.pro@gmail.com>
2016-12-02 22:48:09 +0000

    utilisation d'une template projetSted et debut de configuration

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,11 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss');
 });
+
+elixir(function(mix) {
+    mix.styles([
+        'normalize.css',
+        'main.css'
+    ]);
+});
+
